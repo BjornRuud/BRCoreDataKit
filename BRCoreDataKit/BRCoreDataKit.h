210c456d0a842574150e697c164826b77e258f0c
Bjørn Olav Ruud <b@bjornruud.net>
2014-08-04 15:14:12 +0200

    Initial Commit

---
+++ BRCoreDataKit/BRCoreDataKit.h
@@ -0,0 +1,13 @@
+//
+//  BRCoreDataKit.h
+//  BRCoreDataKit
+//
+//  Created by Bjørn Olav Ruud on 04.08.14.
+//  Copyright (c) 2014 Bjørn Olav Ruud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRCoreDataKit : NSObject
+
+@end
---
+++ BRCoreDataKit/BRCoreDataKit.m
@@ -0,0 +1,13 @@
+//
+//  BRCoreDataKit.m
+//  BRCoreDataKit
+//
+//  Created by Bjørn Olav Ruud on 04.08.14.
+//  Copyright (c) 2014 Bjørn Olav Ruud. All rights reserved.
+//
+
+#import "BRCoreDataKit.h"
+
+@implementation BRCoreDataKit
+
+@end
