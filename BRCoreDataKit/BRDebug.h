c829b8427f5868bde88aae978d589fcfef460da0
Bjørn Olav Ruud <b@bjornruud.net>
2014-08-05 00:32:01 +0200

    Converted DLog and ALog to functions. The preprocessor is evil.

---
+++ BRCoreDataKit/BRDebug.h
@@ -0,0 +1,12 @@
+//
+//  BRDebug.h
+//  BRCoreDataKit
+//
+//  Created by Bjørn Olav Ruud on 05.08.14.
+//  Copyright (c) 2014 Bjørn Olav Ruud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void DLog(NSString *message, ...);
+void ALog(NSString *message, ...);
